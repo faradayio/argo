ad7cadc0a266e4fe13711b31f52f3b4d5aacead4
Bill Morris <wboykinm@geosprocket.com>
2016-02-24 13:22:36 -0500

    collect all manner of metadata

--- argo.js
+++ argo.js
@@ -6,15 +6,20 @@ var request = require('request');
 var rateLimit = require('function-rate-limit');
 
 // params
-var urlBase = 'https://search.mapzen.com/v1/reverse?api_key=';
+var urlBase = 'https://search.mapzen.com/v1/reverse?sources=oa&api_key=';
 var limit = 6;
 var inputFile = process.argv[2];
 var outputFile = process.argv[3];
 var mzKey = process.argv[4];
 
 var failures = 0;
+var successes = 0;
 
-var getAddress = rateLimit(limit, 1000, function (point, callback, attempts) {
+var interval = setInterval(function(){
+  console.log('successes: ' + successes + ' - failures: ' + failures);
+}, 10000);
+
+var getAddress = rateLimit(1000, 1000, function (point, callback, attempts) {
   attempts = attempts || 0;
   if (attempts === 5) {
     failures++;
@@ -36,8 +41,10 @@ var getAddress = rateLimit(limit, 1000, function (point, callback, attempts) {
       var featurePoint = body.features[0].properties;
       point['mz_house_number'] = featurePoint.housenumber;
       point['mz_hnst'] = featurePoint.name;
-      point['mz_city'] = featurePoint.locality;
+      point['mz_city'] = featurePoint.locality || featurePoint.localadmin || '';
       point['mz_state'] = featurePoint.region_a;
+      point['mz_zip'] = featurePoint.postalcode || '';
+      point['mz_label'] = featurePoint.label;
       point['mz_confidence'] = featurePoint.confidence;
       point['mz_distance'] = featurePoint.distance;
       point['mz_source'] = featurePoint.source;
@@ -45,8 +52,10 @@ var getAddress = rateLimit(limit, 1000, function (point, callback, attempts) {
         var featurePoint2 = body.features[1].properties;
         point['mz_backup_house_number'] = featurePoint2.housenumber;
         point['mz_backup_hnst'] = featurePoint2.name;
-        point['mz_backup_city'] = featurePoint2.locality;
+        point['mz_backup_city'] = featurePoint2.locality || featurePoint2.localadmin || '';
         point['mz_backup_state'] = featurePoint2.region_a;
+        point['mz_backup_zip'] = featurePoint2.postalcode || '';
+        point['mz_backup_label'] = featurePoint2.label;
         point['mz_backup_confidence'] = featurePoint2.confidence;
         point['mz_backup_distance'] = featurePoint2.distance;
         point['mz_backup_source'] = featurePoint2.source;
@@ -55,11 +64,14 @@ var getAddress = rateLimit(limit, 1000, function (point, callback, attempts) {
         point['mz_backup_hnst'] = '';
         point['mz_backup_city'] = '';
         point['mz_backup_state'] = '';
+        point['mz_backup_zip'] = '';
+        point['mz_backup_label'] = '';
         point['mz_backup_confidence'] = '';
         point['mz_backup_distance'] = '';
         point['mz_backup_source'] = '';
       }
       callback(null, point);
+      successes++;
     }
   })
 });
@@ -73,4 +85,7 @@ csv
     }
   ))
   .pipe(csv.createWriteStream({headers: true}))
-  .pipe(fs.createWriteStream(outputFile));
+  .pipe(fs.createWriteStream(outputFile))
+  .on('finish', function(){
+    clearInterval(interval);
+  });
